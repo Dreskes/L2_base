fed59be0e063a3da504145194aa35c0ee0cdd141
Timofey Sultanov <sult_wr>
2025-09-12 10:48:43 +0300

    Задание 11

---
+++ 11.js
@@ -0,0 +1,13 @@
+
+function domainName(url) {
+    return url.replace(/https?:\/\//, '')
+              .replace(/www\./, '')
+              .split('.')[0];
+}
+
+
+console.log(domainName("http://github.com/carbonfive/raygun"));
+console.log(domainName("http://www.zombie-bites.com"));         
+console.log(domainName("https://www.cnet.com"));                
+console.log(domainName("www.xakep.ru"));                       
+console.log(domainName("https://youtube.com"));               
