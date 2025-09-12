cf3d76f761d8a8436660b333c4eee382995cd260
Timofey Sultanov <sult_wr>
2025-09-12 10:33:06 +0300

    Задание 10

---
+++ 10.js
@@ -0,0 +1,25 @@
+
+function bingo(ticket, win) {
+    let miniWins = 0;
+    
+    for (let i = 0; i < ticket.length; i++) {
+        let [str, num] = ticket[i];
+        let found = false;
+        
+        for (let j = 0; j < str.length; j++) {
+            if (str.charCodeAt(j) === num) {
+                found = true;
+                break;
+            }
+        }
+        
+        if (found) {
+            miniWins++;
+        }
+    }
+    
+    return miniWins >= win ? "Winner!" : "Loser!";
+}
+
+console.log(bingo([['ABC', 65], ['HGR', 74], ['BYHT', 74]], 1)); 
+console.log(bingo([['ABC', 65], ['HGR', 74], ['BYHT', 74]], 2)); 
