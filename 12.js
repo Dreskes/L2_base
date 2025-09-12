b7c0af5f5787beda13c15ed80a1ed95e12b3f945
Timofey Sultanov <sult_wr>
2025-09-12 10:51:40 +0300

    Задание 12

---
+++ 12.js
@@ -0,0 +1,16 @@
+
+function longest(arr, n) {
+    let indexedArr = arr.map((str, index) => ({ str, index }));
+    
+    indexedArr.sort((a, b) => {
+        if (b.str.length !== a.str.length) {
+            return b.str.length - a.str.length;
+        } else {
+            return a.index - b.index;
+        }
+    });
+    
+    return indexedArr[n - 1].str;
+}
+
+console.log(longest(["Hello", "World", "Codewars", "Katas"], 3)); 
